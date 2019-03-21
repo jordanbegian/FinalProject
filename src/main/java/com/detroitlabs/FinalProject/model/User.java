17a46b8fba78e26ccf9cd1b149a5f82483f7f04a
Brian B <brianb@MacBook-Pro-4.local>
2019-03-21 13:39:17 -0400

    Adds User class and UserRepository that extends JpaRepository

--- src/main/java/com/detroitlabs/FinalProject/model/User.java
+++ src/main/java/com/detroitlabs/FinalProject/model/User.java
@@ -2,14 +2,17 @@ package com.detroitlabs.FinalProject.model;
 
 import com.sun.javafx.beans.IDProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name="users")
 public class User {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
 
     private String name;
 
@@ -17,11 +20,11 @@ public class User {
         this.name = name;
     }
 
-    public int getId() {
+    public UUID getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
