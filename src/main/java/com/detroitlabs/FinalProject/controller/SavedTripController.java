2bbf3fd87be3288e0ee704ea97246b26d10a195c
Brian B <brianb@MacBook-Pro-4.local>
2019-04-02 09:14:24 -0400

    Waypoints pipes are correcly formatted going into database

--- src/main/java/com/detroitlabs/FinalProject/controller/SavedTripController.java
+++ src/main/java/com/detroitlabs/FinalProject/controller/SavedTripController.java
@@ -1,24 +1,13 @@
 package com.detroitlabs.FinalProject.controller;
 
 import com.detroitlabs.FinalProject.model.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.provider.HibernateUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.imageio.spi.ServiceRegistry;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.security.auth.login.Configuration;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @Controller
 public class SavedTripController {
@@ -44,13 +33,9 @@ public class SavedTripController {
     @ResponseBody
     public String createSavedTrip(@PathVariable(name="tripStart") String tripStart, @PathVariable(name="tripEnd") String tripEnd, @PathVariable(name="waypoints") String waypoints) {
 
-        String unformattedWaypoints = waypoints;
-        unformattedWaypoints.replaceAll("-", " ");
-
-        String formattedWaypoints =  unformattedWaypoints.replaceAll(" ", "|");
 
         try {
-            SavedTrip tripToSave = new SavedTrip(1, tripStart, tripEnd, formattedWaypoints);
+            SavedTrip tripToSave = new SavedTrip(1, tripStart, tripEnd, waypoints);
             savedTripsRepository.save(tripToSave);
         }
         catch (Exception ex) {
@@ -77,10 +62,6 @@ public class SavedTripController {
     @ResponseBody
     public Collection<SavedTrip> getByUserid() {
 
-//        String queryString = "FROM SavedTrip WHERE userid = " + userid;
-//        Session session = sessionFactory.openSession();
-//        Query query = session.createQuery(queryString);
-//        List<SavedTrip> returnedTrips = ((org.hibernate.query.Query) query).list();
 
         Collection<SavedTrip> returnedTrips = savedTripsRepository.findTripsByUser();
 
